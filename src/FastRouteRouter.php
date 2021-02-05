d2c9a09349279599b26c14e28170b4fce2234e5f
William Lety <william.lety@gmail.com>
2021-02-05 10:28:24 +0100

    Delete RouteGroups

--- src/FastRouteRouter.php
+++ src/FastRouteRouter.php
@@ -137,13 +137,6 @@ EOT;
     private $routesToInject = [];
 
     /**
-     * RouteGroup
-     *
-     * @var RouteGroup[]
-     */
-    private $groups = [];
-
-    /**
      * Constructor
      *
      * Accepts optionally a FastRoute RouteCollector and a callable factory
@@ -213,53 +206,9 @@ EOT;
         return $route;
     }
 
-    /**
-     * Add RouteGroup
-     *
-     * Ex:
-     * ```
-     * $router->group('/admin', function (RouteGroup $route) {
-     *  $route->route('/acme/route1', 'AcmeController::actionOne', 'route1', [GET]);
-     *  $route->route('/acme/route2', 'AcmeController::actionTwo', 'route2', [GET])->lazyMiddleware(Middleware::class);
-     *  $route->route('/acme/route3', 'AcmeController::actionThree', 'route3', [GET]);
-     * })
-     * ->middleware(Middleware::class);
-     * ```
-     *
-     * @param string $prefix
-     * @param callable $callable
-     * @return RouteGroup
-     */
-    public function group(string $prefix, callable $callable): RouteGroup
-    {
-        $group = new RouteGroup($prefix, $callable, $this);
-        $this->groups[] = $group;
-
-        return $group;
-    }
-
-    /**
-     * Undocumented function
-     *
-     * @param string $prefixPath
-     * @param string|callable $callable
-     * @param string $prefixName
-     * @return RouteGroup
-     */
-    public function crud(string $prefixPath, $callable, string $prefixName): RouteGroup
-    {
-        return $this->group(
-            $prefixPath,
-            function (RouteGroup $route) use ($callable, $prefixName) {
-                $route->crud($callable, $prefixName);
-            }
-        );
-    }
-
     public function match(Request $request): RouteResult
     {
         // Inject any pending routes and groups
-        $this->processGroups();
         $this->injectRoutes();
 
         $dispatchData = $this->getDispatchData();
@@ -295,7 +244,6 @@ EOT;
     public function generateUri(string $name, array $substitutions = [], array $options = []): string
     {
         // Inject any pending routes and groups
-        $this->processGroups();
         $this->injectRoutes();
 
         if (!isset($this->routes[$name])) {
@@ -515,22 +463,6 @@ EOT;
         }
 
         $this->router->addRoute($methods, $route->getPath(), $route->getPath());
-    }
-
-    /**
-     * Process all groups
-     *
-     * Adds all of the group routes to the collection and determines if the group
-     * strategy should be be used.
-     *
-     * @return void
-     */
-    protected function processGroups(): void
-    {
-        foreach ($this->groups as $key => $group) {
-            unset($this->groups[$key]);
-            $group();
-        }
     }
 
     /**
